da2f6d0b90a12170588fab24f655f4fbe8e4514f
Pogopin <chegarakafree@gmail.com>
2023-12-07 11:49:38 +0300

    upload

---
+++ src/assets/js/chartOption.js
@@ -0,0 +1,44 @@
+export default function setEchartProp (m, g) {
+	let option = {
+		tooltip: {
+			trigger: 'item'
+		},
+		legend: {
+			top: '5%',
+			left: 'center'
+		},
+		series: [
+			{
+				name: 'Access From',
+				type: 'pie',
+				radius: ['40%', '70%'],
+				avoidLabelOverlap: false,
+				itemStyle: {
+					borderRadius: 10,
+					borderColor: '#fff',
+					borderWidth: 2
+				},
+				label: {
+					show: false,
+					position: 'center'
+				},
+				emphasis: {
+					label: {
+						show: true,
+						fontSize: 40,
+						fontWeight: 'bold'
+					}
+				},
+				labelLine: {
+					show: false
+				},
+				data: [
+					{ value: m, name: 'Мужчины' },
+					{ value: g, name: 'Женщины' },
+
+				]
+			}
+		]
+	}
+	return { option }
+}
---
+++ src/views/HomeView.vue
@@ -0,0 +1,94 @@
+<script setup>
+import { onBeforeMount, ref, watch, computed, onMounted } from 'vue';
+import _ from 'lodash';
+import * as echarts from 'echarts';
+import setEchartProp from '@/assets/js/chartOption.js';
+
+const APP_ID = '51809385';
+const version = '5.199';
+
+const members = ref([]);
+const chartElementRef = ref();
+let chart;
+
+const chartOptions = computed(() => {
+  let m = members.value.filter((el) => el.sex == 2).length;
+  let g = members.value.filter((el) => el.sex == 1).length;
+  
+  const { option } = setEchartProp(m,g);  
+  return option
+})
+
+const fetchMembers = _.debounce(async () => {
+  await VK.Api.call('groups.getMembers', {
+    group_id: 'dendynn',
+    v: version,
+    language: 'ru',
+    fields: 'photo_200_orig,sex,city',
+    count: 200
+  }, r => {
+      members.value = r.response.items;      
+    })
+}, 300)
+
+onBeforeMount(async () => {
+  await VK.init({
+    apiId: APP_ID
+  });
+  // await VK.Auth.login();
+  fetchMembers();
+})
+onMounted(() => {    
+  chart = echarts.init(chartElementRef.value);
+  // chart.setOption(chartOptions.value)
+})
+
+watch(chartOptions, () => {
+  chart.setOption(chartOptions.value)
+  
+})
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="members">
+      <div v-for="item in members" :key="item.id" :class="['members__item', {'members__item_girl' : item.sex == 1}]" >
+        <img :src="item.photo_200_orig" alt="">
+      </div>
+    </div>
+    <div class="grafic" ref="chartElementRef">
+    </div>
+  </div>
+</template>
+<style scoped>
+.container {
+  max-width: 1300px;
+  margin: 0 auto;
+}
+.members {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr) );
+  gap: 5px;
+}
+.members__item {
+  border: 3px solid grey;
+}
+.members__item_girl {
+  border: 3px solid pink;
+}
+.members__item img{
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+}
+.grafic {
+  position: fixed !important;
+  right: 0;
+  top: 0;
+  z-index: 2;
+  width: 300px;
+  height: 300px;
+  background-color: yellow;
+}
+</style>
